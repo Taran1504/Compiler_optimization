0d1846faf1a7b5bc1d2190c39085e7c1068b320c
Taranjeet Singh <124021531+Taran1504@users.noreply.github.com>
2025-06-23 10:56:08 +0530

    Update simple_loops.cc

--- simple_loops.cc
+++ simple_loops.cc
@@ -1,13 +1,35 @@
 #include <benchmark/benchmark.h>
 
 static void BM_SimpleLoop(benchmark::State& state) {
+    const int size = state.range(0);
     for (auto _ : state) {
         int sum = 0;
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < size; ++i) {
             sum += i;
         }
         benchmark::DoNotOptimize(sum);
     }
 }
-BENCHMARK(BM_SimpleLoop);
+BENCHMARK(BM_SimpleLoop)->Arg(1000)->Arg(10000)->Arg(100000);
+
+static void BM_SimpleLoopWithPointer(benchmark::State& state) {
+    const int size = state.range(0);
+    int* array = new int[size];
+    for (int i = 0; i < size; ++i) {
+        array[i] = i;
+    }
+    
+    for (auto _ : state) {
+        int sum = 0;
+        int* ptr = array;
+        for (int i = 0; i < size; ++i) {
+            sum += *ptr++;
+        }
+        benchmark::DoNotOptimize(sum);
+    }
+    
+    delete[] array;
+}
+BENCHMARK(BM_SimpleLoopWithPointer)->Arg(1000)->Arg(10000)->Arg(100000);
+
 BENCHMARK_MAIN();
