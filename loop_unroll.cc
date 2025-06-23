ee557775a1e5559157afaa09bdf9076edc1db672
Taranjeet Singh <124021531+Taran1504@users.noreply.github.com>
2025-06-23 10:51:34 +0530

    Update loop_unroll.cc

--- loop_unroll.cc
+++ loop_unroll.cc
@@ -3,6 +3,7 @@
 static void BM_LoopUnroll(benchmark::State& state) {
     for (auto _ : state) {
         int sum = 0;
+        // Manual unrolling
         for (int i = 0; i < 1000; i += 5) {
             sum += i;
             sum += i + 1;
@@ -14,4 +15,18 @@ static void BM_LoopUnroll(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_LoopUnroll);
+
+static void BM_LoopUnrollPragma(benchmark::State& state) {
+    for (auto _ : state) {
+        int sum = 0;
+        // Compiler-assisted unrolling
+        #pragma unroll(5)
+        for (int i = 0; i < 1000; ++i) {
+            sum += i;
+        }
+        benchmark::DoNotOptimize(sum);
+    }
+}
+BENCHMARK(BM_LoopUnrollPragma);
+
 BENCHMARK_MAIN();
