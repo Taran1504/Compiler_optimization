9f6e46974d156d4e1a951671b79a113c44f9d9e0
Taranjeet Singh <124021531+Taran1504@users.noreply.github.com>
2025-06-23 10:49:33 +0530

    Update conditional_loops.cc

--- conditional_loops.cc
+++ conditional_loops.cc
@@ -1,18 +1,43 @@
 #include <benchmark/benchmark.h>
 
 static void BM_ConditionalLoop(benchmark::State& state) {
-  int sum = 0;
-  for (auto _ : state) {
-    for (int i = 0; i < 1024; ++i) {
-      if (i % 2 == 0) {
-        sum += i;
-      } else {
-        sum -= i;
-      }
+    const int size = state.range(0);
+    int sum = 0;
+    for (auto _ : state) {
+        for (int i = 0; i < size; ++i) {
+            if (i % 2 == 0) [[likely]] {
+                sum += i;
+            } else [[unlikely]] {
+                sum -= i;
+            }
+        }
+        benchmark::DoNotOptimize(sum);
     }
-    benchmark::DoNotOptimize(sum);
-  }
 }
-BENCHMARK(BM_ConditionalLoop);
+BENCHMARK(BM_ConditionalLoop)->Arg(1024)->Arg(2048)->Arg(4096);
+
+static void BM_ConditionalLoopNoBranch(benchmark::State& state) {
+    const int size = state.range(0);
+    int sum = 0;
+    for (auto _ : state) {
+        for (int i = 0; i < size; ++i) {
+            sum += (i % 2 == 0) ? i : -i;
+        }
+        benchmark::DoNotOptimize(sum);
+    }
+}
+BENCHMARK(BM_ConditionalLoopNoBranch)->Arg(1024)->Arg(2048)->Arg(4096);
+
+static void BM_ConditionalLoopBitwise(benchmark::State& state) {
+    const int size = state.range(0);
+    int sum = 0;
+    for (auto _ : state) {
+        for (int i = 0; i < size; ++i) {
+            sum += ((i & 1) - 1) & ~i | (~(i & 1) + 1) & i;
+        }
+        benchmark::DoNotOptimize(sum);
+    }
+}
+BENCHMARK(BM_ConditionalLoopBitwise)->Arg(1024)->Arg(2048)->Arg(4096);
 
 BENCHMARK_MAIN();
