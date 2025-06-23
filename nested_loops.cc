dd0ed831b8033ccd6e81666f17b215bc97c626a6
Taranjeet Singh <124021531+Taran1504@users.noreply.github.com>
2025-06-23 10:54:29 +0530

    Update nested_loops.cc

--- nested_loops.cc
+++ nested_loops.cc
@@ -1,25 +1,29 @@
 #include <benchmark/benchmark.h>
 
 static void BM_NestedLoop(benchmark::State& state) {
-  int sum = 0;
-  for (auto _ : state) {
-    for (int i = 0; i < 128; ++i) {
-      for (int j = 0; j < 128; ++j) {
-        sum += i * j;
-      }
+    int sum = 0;
+    const int size = state.range(0);
+    for (auto _ : state) {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                sum += i * j;
+            }
+        }
+        benchmark::DoNotOptimize(sum);
     }
-    benchmark::DoNotOptimize(sum);
-  }
 }
-BENCHMARK(BM_NestedLoop);
+BENCHMARK(BM_NestedLoop)->Arg(128)->Arg(256)->Arg(512);
 
 static void BM_NestedLoopTiled(benchmark::State& state) {
     int sum = 0;
-    for(auto _ : state){
-        for(int ii = 0; ii < 128; ii += 32){
-            for(int jj = 0; jj < 128; jj+=32){
-                for(int i = ii; i < ii + 32; ++i){
-                    for(int j = jj; j < jj + 32; ++j){
+    const int size = state.range(0);
+    const int tile_size = state.range(1);
+    
+    for (auto _ : state) {
+        for (int ii = 0; ii < size; ii += tile_size) {
+            for (int jj = 0; jj < size; jj += tile_size) {
+                for (int i = ii; i < ii + tile_size && i < size; ++i) {
+                    for (int j = jj; j < jj + tile_size && j < size; ++j) {
                         sum += i * j;
                     }
                 }
@@ -28,6 +32,6 @@ static void BM_NestedLoopTiled(benchmark::State& state) {
         benchmark::DoNotOptimize(sum);
     }
 }
-BENCHMARK(BM_NestedLoopTiled);
+BENCHMARK(BM_NestedLoopTiled)->Args({128, 16})->Args({128, 32})->Args({256, 32})->Args({256, 64});
 
 BENCHMARK_MAIN();
