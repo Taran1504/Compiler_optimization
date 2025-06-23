cbc3c78aed15fac558bfbf742e041745090add16
Taranjeet Singh <124021531+Taran1504@users.noreply.github.com>
2025-06-23 10:50:52 +0530

    Update instruction_scheduling.cc

--- instruction_scheduling.cc
+++ instruction_scheduling.cc
@@ -16,4 +16,36 @@ static void BM_InstructionScheduling(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_InstructionScheduling);
+
+static void BM_InstructionSchedulingWithDeps(benchmark::State& state) {
+    for (auto _ : state) {
+        int a = 1;
+        for (int i = 0; i < 1000; ++i) {
+            a += i;
+            a *= i + 1;
+            a ^= i;
+            a -= i;
+        }
+        benchmark::DoNotOptimize(a);
+    }
+}
+BENCHMARK(BM_InstructionSchedulingWithDeps);
+
+static void BM_InstructionParallel(benchmark::State& state) {
+    for (auto _ : state) {
+        int a = 1, b = 2, c = 3, d = 4;
+        int i = state.range(0);
+        // Independent operations
+        a += i;
+        b *= i + 1;
+        c ^= i;
+        d -= i;
+        benchmark::DoNotOptimize(a);
+        benchmark::DoNotOptimize(b);
+        benchmark::DoNotOptimize(c);
+        benchmark::DoNotOptimize(d);
+    }
+}
+BENCHMARK(BM_InstructionParallel)->Arg(1000);
+
 BENCHMARK_MAIN();
